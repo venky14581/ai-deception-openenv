750cd61d92d24428fd0f397c700ac3b6c7decdfd
spidey121 <jayasaivenkateshkarnasula@gmail.com>
2026-04-12 13:34:57 +0000

    fix round 1

--- inference.py
+++ inference.py
@@ -9,9 +9,6 @@ from openai import OpenAI
 random.seed(42)
 
 # Start fake server
-from env.fake_server import run_server
-threading.Thread(target=run_server, daemon=True).start()
-time.sleep(2)
 
 from env.env import DeceptionEnv
 from env.attacker import simulate_attack
